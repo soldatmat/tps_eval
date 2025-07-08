a5127da0f17af7215976cd291c2aee6521e80063
Matouš Soldát <matoussoldat@email.cz>
2025-07-08 14:45:55 +0200

    Set default resources to lower option

--- scripts/aurum/jobs/max_sequence_identity.sh
+++ scripts/aurum/jobs/max_sequence_identity.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
 #SBATCH -J max_sequence_identity
-#SBATCH --time=0-11:59:00
+#SBATCH --time=0-3:59:00
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
-#SBATCH --mem=180G
-#SBATCH --partition=a36_192_long
+#SBATCH --mem=32G
+#SBATCH --partition=a36_any
 
-# For smaller number of sequences, partition a36_any can be used:
+# For smaller number of sequences (1000+ is ok), partition a36_any can be used:
 # --time=0-03:59:00
 # --mem=32G
 # --partition=a36_any
+
+# For larger number of sequences (e.g. 10_000):
+# --time=0-11:59:00
+# --mem=180G
+# --partition=a36_192_long
 
 # Usage: sbatch max_sequence_identity.sh --fasta_path <fasta_path> [--train_path <train_path> --train]
 
