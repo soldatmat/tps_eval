d205ac132b67db88782d3a219b732edbecf44017
Matouš Soldát <matoussoldat@email.cz>
2025-07-08 13:12:57 +0200

    Increase max_sequence_identity resources

--- scripts/aurum/jobs/max_sequence_identity.sh
+++ scripts/aurum/jobs/max_sequence_identity.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 #SBATCH -J max_sequence_identity
-#SBATCH --time=0-03:59:00
+#SBATCH --time=0-11:59:00
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
-#SBATCH --mem=32G
-#SBATCH --partition=a36_any
+#SBATCH --mem=180G
+#SBATCH --partition=a36_192_long
+
+# For smaller number of sequences, partition a36_any can be used:
+# --time=0-03:59:00
+# --mem=32G
+# --partition=a36_any
 
 # Usage: sbatch max_sequence_identity.sh --fasta_path <fasta_path> [--train_path <train_path>]
 
