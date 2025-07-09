dbe748b81da00d50b27ec59929a3e23c93ef54cb
soldatmat <matoussoldat@email.cz>
2025-07-09 13:37:39 +0200

    Reduce RAM usage & extract hit IDs

--- src/sequence_metrics/max_sequence_identity.jl
+++ src/sequence_metrics/max_sequence_identity.jl
@@ -12,47 +12,56 @@ SUBSTITUTION_MATRIX = BLOSUM62
 function evaluate_max_sequence_identity(
     train,
     generated,
-    generated_identifiers;
+    generated_identifiers,
+    train_identifiers;
     save_path::Union{String,Nothing}=nothing,
     return_second_max::Bool=false
 )
-    # --- Maximum sequence identity to any train sequence --------------------------------------------------
-    sequence_identity, sequence_similarity = get_pairwise_sequence_identity_with_pairwise_alignment(train, generated)
+    max_sequence_identity, max_sequence_similarity, max_sequence_identity_index, max_sequence_similarity_index = get_max_sequence_identity(train, generated; self_comparison=return_second_max)
 
-    # Get maximum sequence identity per generated sequence
-    get_max_identity(sequence_identity) = map(identities -> maximum(identities), eachcol(sequence_identity))
-    get_second_max_identity(sequence_identity) = getindex.(sort.(eachcol(sequence_identity), rev=true), 2)
-
-    max_sequence_identity = return_second_max ? get_second_max_identity(sequence_identity) : get_max_identity(sequence_identity)
-    max_sequence_similarity = return_second_max ? get_second_max_identity(sequence_similarity) : get_max_identity(sequence_similarity)
+    max_sequence_identity_hit = train_identifiers[max_sequence_identity_index]
+    max_sequence_similarity_hit = train_identifiers[max_sequence_similarity_index]
 
     # Save calculated novelty into CSV
     if !isnothing(save_path)
         df = DataFrame(
             :ID => generated_identifiers,
             :sequence_identity => max_sequence_identity,
+            :sequence_identity_hit => max_sequence_identity_hit,
             :sequence_similarity => max_sequence_similarity,
+            :sequence_similarity_hit => max_sequence_similarity_hit,
         )
         CSV.write(save_path, df)
     end
 
-    return generated_identifiers, max_sequence_identity, max_sequence_similarity
+    return max_sequence_identity, max_sequence_similarity, max_sequence_identity_hit, max_sequence_similarity_hit
 end
 
 """
 Computes sequence identity between each pair of sequences from `train` and `generated`
+Returns max sequence identity for each `generated` sequence with any of the `train` sequences.
 
 Sequence identity implementation:
 - constructs pairwise alignment for each pair of sequences
 - identity = #Matches / length(alignment)
 """
-function get_pairwise_sequence_identity_with_pairwise_alignment(train::AbstractVector{LongAA}, generated::AbstractVector{LongAA})
+get_max_sequence_identity(sequences) = get_max_sequence_identity(sequences, sequences; self_comparison=true)
+
+function get_max_sequence_identity(train, generated; self_comparison::Bool=false)
+    # Convert sequences to ::LongAA
+    train = LongAA.(train)
+    generated = LongAA.(generated)
+    get_max_sequence_identity(train, generated; self_comparison)
+end
+
+function get_max_sequence_identity(
+    train::AbstractVector{LongAA},
+    generated::AbstractVector{LongAA};
+    self_comparison::Bool=false,
+)
     # Construct pairwise alignments of each generated sequence to each train sequence
     score_model = AffineGapScoreModel(SUBSTITUTION_MATRIX, gap_open=-11, gap_extend=-1)
     align_sequences(seq1::LongAA, seq2::LongAA) = pairalign(GlobalAlignment(), seq1, seq2, score_model)
-    sequence_alignments = mapreduce(generated_seq -> align_sequences.(Ref(generated_seq), train), hcat, generated)
-    # `sequence_alignments` is length(train)×length(generated) Matrix{PairwiseAlignmentResult{Int64, LongAA, LongAA}}
-    # -> sequence_alignments[:,1] are pairwise alignments of 1st generated sequence to each train sequence
 
     # Calculate sequence identities from pairwise alignments
     get_denominator(a::T) where {T<:PairwiseAlignmentResult} = (a |> alignment |> length) # Alignment length
@@ -61,17 +70,30 @@ function get_pairwise_sequence_identity_with_pairwise_alignment(train::AbstractV
     #get_sequence_similarit(a::T) where {T<:PairwiseAlignmentResult} = (a)
     get_sequence_similarity(a::T) where {T<:PairwiseAlignmentResult} = count_positives(a, SUBSTITUTION_MATRIX) / get_denominator(a)
 
-    sequence_identity = get_sequence_identity.(sequence_alignments)
-    sequence_similarity = get_sequence_similarity.(sequence_alignments)
+    max_sequence_identity = zeros(Float64, length(generated))
+    max_sequence_similarity = zeros(Float64, length(generated))
+    max_sequence_identity_index = zeros(Int, length(generated))
+    max_sequence_similarity_index = zeros(Int, length(generated))
+    for (i, generated_seq) in enumerate(generated)
+        for (j, train_seq) in enumerate(train)
+            ((self_comparison) && (i == j)) && continue # Skip self-comparison
 
-    return sequence_identity, sequence_similarity
-end
+            sequence_alignment = align_sequences(generated_seq, train_seq)
+            sequence_identity = get_sequence_identity(sequence_alignment)
+            if sequence_identity > max_sequence_identity[i]
+                max_sequence_identity[i] = sequence_identity
+                max_sequence_identity_index[i] = j
+            end
 
-function get_pairwise_sequence_identity_with_pairwise_alignment(train, generated)
-    # Convert sequences to ::LongAA
-    train = LongAA.(train)
-    generated = LongAA.(generated)
-    get_pairwise_sequence_identity_with_pairwise_alignment(train, generated)
+            sequence_similarity = get_sequence_similarity(sequence_alignment)
+            if sequence_similarity > max_sequence_similarity[i]
+                max_sequence_similarity[i] = sequence_similarity
+                max_sequence_similarity_index[i] = j
+            end
+        end
+    end
+
+    return max_sequence_identity, max_sequence_similarity, max_sequence_identity_index, max_sequence_similarity_index
 end
 
 #= function get_ncbi_blast_sequence_identity(train_path, generated_path)
@@ -116,7 +138,7 @@ function main_generated_sequences(;
     generated_identifiers, generated = separate_identifiers(load_fasta_sequences(fasta_path; load_identifiers=true))
     train_identifiers, train = separate_identifiers(load_fasta_sequences(train_path; load_identifiers=true))
     save_path = _get_save_path(fasta_path)
-    evaluate_max_sequence_identity(train, generated, generated_identifiers; save_path)
+    evaluate_max_sequence_identity(train, generated, generated_identifiers, train_identifiers; save_path)
 end
 
 ########## Main without reference sequences ##########
@@ -126,5 +148,5 @@ function main_train_sequences(train_path::String)
 end
 function _main_train_sequences(train_path::String, train, train_identifiers)
     save_path = _get_save_path(train_path; save_suffix="self")
-    evaluate_max_sequence_identity(train, train, train_identifiers; save_path, return_second_max=true)
+    evaluate_max_sequence_identity(train, train, train_identifiers, train_identifiers; save_path, return_second_max=true)
 end
