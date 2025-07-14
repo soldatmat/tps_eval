1ce70d130a2eb3c6b1d593a94874d1bf304416fa
Matouš Soldát <matoussoldat@email.cz>
2025-07-14 10:53:15 +0200

    Increase time and cpus

--- scripts/aurum/jobs/max_sequence_identity.sh
+++ scripts/aurum/jobs/max_sequence_identity.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 #SBATCH -J max_sequence_identity
-#SBATCH --time=0-03:59:00
+#SBATCH --time=0-11:59:00
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --mem=32G
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=10
-#SBATCH --partition=a36_any
+#SBATCH --cpus-per-task=20
+#SBATCH --partition=a36_96_long
 
 # Usage: sbatch max_sequence_identity.sh --fasta_path <fasta_path> [--train_path <train_path> --train]
 
@@ -15,4 +15,4 @@ SCRIPT_PATH=$(scontrol show job "$SLURM_JOB_ID" | awk -F= '/Command=/{print $2}'
 
 cd $(dirname "$SCRIPT_PATH")/../..
 
-JULIA_NUM_THREADS=10 sh run_max_sequence_identity.sh "$@"
+JULIA_NUM_THREADS=20 sh run_max_sequence_identity.sh "$@"
