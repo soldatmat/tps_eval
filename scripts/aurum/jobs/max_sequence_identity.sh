0e2fd72ddc83c80c769f0c4481a0aa1d0bef1536
Matouš Soldát <matoussoldat@email.cz>
2025-09-08 14:18:11 +0200

    Reduce max_sequence_identity resources

--- scripts/aurum/jobs/max_sequence_identity.sh
+++ scripts/aurum/jobs/max_sequence_identity.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 #SBATCH -J max_sequence_identity
-#SBATCH --time=0-11:59:00
+#SBATCH --time=0-3:59:00
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --mem=32G
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=20
-#SBATCH --partition=a36_96_long
+#SBATCH --partition=a36_96
 
 # Usage: sbatch max_sequence_identity.sh --fasta_path <fasta_path> [--train_path <train_path> --train]
 
