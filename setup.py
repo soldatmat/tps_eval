b0d5b8f70707f7c3e01aa31681768e1601c18263
Matouš Soldát <matoussoldat@email.cz>
2026-01-12 13:09:18 +0100

    Make source code importable with pip

---
+++ setup.py
@@ -0,0 +1,6 @@
+from setuptools import setup
+
+setup(
+    name="tps_eval",
+    packages=["src"],
+)
--- setup.sh
+++ setup.sh
@@ -5,3 +5,4 @@ conda create -n tps_eval -c conda-forge python biopython pandas requests tqdm -y
 conda activate tps_eval
 pip install torch
 pip install fair-esm
+pip install -e .
